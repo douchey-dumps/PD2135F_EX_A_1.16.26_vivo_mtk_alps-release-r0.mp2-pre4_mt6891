70eb8c941698825ab073c0895dd99d2702fc0677
IC1101 <malabananic1@gmail.com>
2021-12-01 14:00:53 +0000

    add vendor

---
+++ vendor/camera3rd/nti/ProductConfig.mk
@@ -0,0 +1,253 @@
+#--------------------------------------------------------------
+# algo : zoom
+ALGO_ZOOM_LIB_COMPLIE                      := 1
+ALGO_ZOOM_NOW_SUPPORT                      := 0
+ALGO_ZOOM_EX_SUPPORT                       := 0
+ALGO_ZOOM_VIVO_SUPPORT                     := 1
+
+#--------------------------------------------------------------
+# algo : video/preview singleblur
+ALGO_VIDEO_SINGLEBLUR_LIB_COMPLIE          := 1
+
+#--------------------------------------------------------------
+# algo : singleblur
+ALGO_SINGLEBLUR_LIB_COMPLIE                := 1
+ALGO_SINGLEBLUR_VERSION_CONTROL            := 1
+
+#--------------------------------------------------------------
+# algo : relight
+ALGO_VIVORELIGHT_LIB_COMPLIE               := 1
+
+#-----------------------------------------------------------
+# algo : portraitlight
+ALGO_PORTRAITLIGHT_LIB_COMPLIE             := 1
+
+#--------------------------------------------------------------
+# algo : supserns
+ALGO_SUPERNS_LIB_COMPLIE                   := 0
+ALGO_NEW_SUPERNS_PROCESS                   := 0
+
+#--------------------------------------------------------------
+# algo : supsernsNR
+ALGO_SUPERNSNR_LIB_COMPLIE                 := 0
+#-----------------------------------------------------------
+
+# algo : ainr
+ALGO_AINR_LIB_COMPLIE                      := 0
+
+#--------------------------------------------------------------
+# algo : deflicker
+ALGO_DEFLICKER_LIB_COMPLIE                 := 0
+
+# algo : rawllhdr
+ALGO_RAWLLHDR_LIB_COMPLIE                  := 0
+ALGO_RAWLLHDR_SUPERNS_DEPART               := 0
+ALGO_RAWLLHDR_USE_RAWNR_RK                 := 0
+ALGO_RAWLLHDR_SUPERNS_DEPART_THREE_EXPO    := 0
+
+#--------------------------------------------------------------
+# algo : rawhdr
+ALGO_RAWHDR_LIB_COMPLIE                    := 1
+ALGO_RAWHDR_LIB_VERSION_CONTROL            := 2
+
+#-----------------------------------------------------------
+# algo : super star
+ALGO_SUPERSTAR_LIB_COMPLIE                 := 1
+ALGO_SUPERSTAR_LIB_VERSION_CONTROL         := 2
+
+#--------------------------------------------------------------
+# algo : prebps
+ALGO_PREBPS_LIB_COMPLIE                    := 1
+
+#--------------------------------------------------------------
+# algo : preview detect
+ALGO_PREVIEW_DETECT_LIB_COMPLIE            := 1
+
+#-------------------------------------------------------------
+# algo : night
+ALGO_NIGHT_LIB_COMPLIE                     := 0
+
+#--------------------------------------------------------------
+#algo: night portrait
+ALGO_NIGHTPORTRAIT_LIB_COMPLIE             := 1
+
+#--------------------------------------------------------------
+# algo : lut
+ALGO_LUT_LIB_COMPLIE                       := 1
+
+#--------------------------------------------------------------
+# algo : hdr
+ALGO_HDR_LIB_COMPLIE                       := 1
+
+#--------------------------------------------------------------
+# algo : dualbokeh preview
+ALGO_DUALBOKEH_PREVIEW_LIB_COMPLIE         := 1
+ALGO_DUALBOKEH_PREVIEW_VIVO_SUPPORT        := 1
+
+#--------------------------------------------------------------
+# algo : dualbokeh shot
+ALGO_DUALBOKEH_LIB_COMPLIE                 := 1
+ALGO_DUALBOKEH_VIVO_SUPPORT                := 1
+
+#-----------------------------------------------------------
+# algo : 光斑是否使用2.0版本，模式是3.0以上
+# 如果应用需要使用2.0的，UI界面是心形，需要配置成1
+ALGO_DUALBOKEH_FACULA_VERSION2          := 0
+
+#--------------------------------------------------------------
+# algo : distort
+ALGO_DISTORTION_LIB_COMPLIE                := 1
+ALGO_DISTORTION_ALTEK_VERSION_CONTROL      := 0
+ALGO_DISTORTION_VIVO_SUPPORT               := 1
+
+#--------------------------------------------------------------
+# algo : beautybody
+ALGO_BEAUTYBODY_LIB_COMPLIE                := 0
+
+#--------------------------------------------------------------
+# algo : beauty
+ALGO_BEAUTY_LIB_COMPLIE                    := 1
+ALGO_BEAUTY_VIVO_VERSION_CONTROL           := 4
+
+#-----------------------------------------------------------
+# algo : sat
+ALGO_SAT_LIB_COMPLIE                       := 1
+ALGO_SAT_VENDOR_VIVO_SUPPORT               := 1
+ALGO_FUSION_VENDOR_VIVO_SUPPORT            := 1
+
+#-----------------------------------------------------------
+# algo : aidetect
+ALGO_PREVIEW_AIDETECT_LIB_COMPLIE          := 1
+
+#-----------------------------------------------------------
+# algo : movieportrait
+ALGO_MOVIEPORTRAIT_LIB_COMPLIE             := 1
+ALGO_MOVIEPORTRAIT_LIB_NPU                 := 1
+
+#-----------------------------------------------------------
+# algo : palm detect
+ALGO_PALM_DETECT_LIB_COMPLIE               	:= 1
+ALGO_PALMDETECT_VENDOR_SENSETIME_SUPPORT  	:= 0
+ALGO_PALMDETECT_VENDOR_VIVO_SUPPORT       	:= 1
+ALGO_PALMDETECT_VENDOR_VIVO_CPUVERSION  	:= 1
+ALGO_PALMDETECT_VENDOR_VIVO_GPUVERSION      := 0
+
+#-----------------------------------------------------------
+# algo : eye detect
+ALGO_EYE_DETECT_LIB_COMPLIE                := 1
+
+#-----------------------------------------------------------
+# algo : lot detect
+ALGO_LOT_DETECT_LIB_COMPLIE                := 1
+
+#-----------------------------------------------------------
+# algo : eye detect
+
+#-----------------------------------------------------------
+# algo : humanbody detect
+ALGO_HUMANBODY_DETECT_LIB_COMPLIE          := 1
+
+#--------------------------------------------------------------
+# algo : engine distor
+ALGO_ENGINE_DISTORTION_LIB_COMPLIE         := 0
+
+#-----------------------------------------------------------
+# algo : engine dualbokeh
+ALGO_ENGINE_DUALBOKEH_CALIB_LIB_COMPLIE        := 1
+ALGO_ENGINE_DUALBOKEH_CALIB_VIVO_SUPPORT       := 1
+
+#-----------------------------------------------------------
+# algo : remosaic
+ALGO_REMOSAIC_LIB_COMPLIE                  :=0
+ALGO_REMOSAIC_SAMSUNG3p9_LIB_COMPLIE       :=0
+ALGO_REMOSAIC_RK_LIB_COMPLIE               :=0
+ALGO_REMOSAIC_RK_CPU_VERSION               :=0
+ALGO_REMOSAIC_RK_DSP_VERSION               :=0
+
+#-----------------------------------------------------------
+# algo : check parameter
+ALGO_CHECK_PARAMETER                       :=0
+
+#-----------------------------------------------------------
+# algo : portraitstyle
+ALGO_PORTRAITSTYLE_LIB_COMPLIE             := 1
+
+#--------------------------------------------------------------
+# algo : rawnr
+ALGO_RAWNR_LIB_COMPLIE                     := 1
+ALGO_GRA_LIB_COMPLIE                       := 1
+
+#-----------------------------------------------------------
+# algo : motion denoise
+ALGO_MOTION_DENOISE_LIB_COMPLIE                := 1
+ALGO_MOTION_DENOISE_FRAME_BY_FRAME_LIB_COMPLIE := 0
+ALGO_MOTION_DENOISE_VERSION_CONTROL            := 1.5
+
+#-----------------------------------------------------------
+# algo : sueprmoon
+ALGO_SUPERMOON_LIB_COMPLIE                 := 1
+ALGO_SUPERMOON_LIB_VERSION_CONTROL         := 2
+
+#-----------------------------------------------------------
+# algo : double exposure
+ALGO_DOUBLE_EXPO_LIB_COMPLIE                 := 1
+
+#-----------------------------------------------------------
+# algo : fd detect algorithm
+ALGO_THIRD_PART_ALOG_FD_ALGORITHM_LIB_COMPLIE            :=1
+ALGO_VIVO_FD_ALGORITHM_LIB_COMPLIE                       :=1
+
+#-----------------------------------------------------------
+# pipeline shot
+SHOT_USE_VAS_PIPELINE                        := 0
+
+#-----------------------------------------------------------
+# algo : faceEnhance
+ALGO_FACE_ENHANCE_LIB_COMPLIE             := 1
+
+#-----------------------------------------------------------
+# algo : document detect
+ALGO_DOCUMENT_DETECT_LIB_COMPLIE		:= 1
+
+#-----------------------------------------------------------
+# algo : video_nr
+ALGO_VIDEONR_LIB_COMPLIE                   := 1
+ALGO_VIDEONR_LIB_VERSION_CONTROL           := 2
+
+# algo : outline
+ALGO_OUTLINE_LIB_COMPLIE             := 1
+ALGO_OUTLINE_USE_TRUESIGHT           := 1
+
+#-----------------------------------------------------------
+# algo : aienhance satfusion场景，流程zoom算法->aienhance算法
+ALGO_AIENHANCE_LIB_COMPLIE                 := 1
+
+#-----------------------------------------------------------
+# algo : faceDetectCapture
+ALGO_FACEDETECT_CAPTURE_LIB_COMPLIE  := 1
+
+# algo : softbrp
+ALGO_SOFTBRP_LIB_COMPLIE                := 1
+#
+# # algo :rawenhance
+ALGO_RAWENHANCE_LIB_COMPLIE             := 0
+
+#--------------------------------------------------------------
+# algo : AIDEGLARE
+ALGO_AIDEGLARE_LIB_COMPLIE             := 1
+
+#-----------------------------------------------------------
+# algo : 后置人像模式4.0功能， 应用打开了星光镜功能需要打开这个检测，默认关闭
+ALGO_SUPPORT_BRIGHTSPOT_DETECT          := 1
+
+#-----------------------------------------------------------
+# algo : product num
+ALGO_CAMERA3RD_PRODUCT_NUM                   := 0x2135F
+
+#-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+# project algo config date, compare with algo overdue end date
+# config date > date today or config date < 2021-01-01, not reasonable,
+# project compile abort.
+# when overdue end date < project algo config date, algo not supported,
+# project compile abort.
+ALGO_PROJECT_CONFIG_DATE_COMPLIE             := 2021-05-17
